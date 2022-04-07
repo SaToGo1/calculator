46c62626ebaecc1edba67e78b820cce593cf9c37
SaToGo <stoledano97@gmail.com>
2022-04-07 14:41:17 +0200

    onclick event bug fixed

--- myScript.js
+++ myScript.js
@@ -19,9 +19,40 @@ function operate(operation, a, b){
     return operation(a, b);
 }
 
-//We use value as a string.
 let displayValue = "";
+//value --> string that contains a number.
 function saveNumbers(value){
     displayValue = displayValue + value
 }
 
+ let displayResult = document.getElementById("display_result");
+function actualizeDisplay(){
+    displayResult.textContent = displayValue;
+}
+
+
+document.getElementById("number_7").onclick= () => saveNumbers('7');
+document.getElementById("number_8").onclick= () => saveNumbers('8');
+document.getElementById("number_9").onclick= () => saveNumbers('9');
+
+document.getElementById("number_4").onclick= () => saveNumbers('4');
+document.getElementById("number_5").onclick= () => saveNumbers('5');
+document.getElementById("number_6").onclick= () => saveNumbers('6');
+
+document.getElementById("number_1").onclick= () => saveNumbers('1');
+document.getElementById("number_2").onclick= () => saveNumbers('2');
+document.getElementById("number_3").onclick= () => saveNumbers('3');
+
+//document.getElementById("#number_point").onclick=...;
+//document.getElementById("#number_equal").onclick=...;
+
+//document.getElementById("#operation_clear").onclick=...;
+//document.getElementById("#operation_plus").onclick=...;
+//document.getElementById("#operation_minus").onclick=...;
+//document.getElementById("#operation_multiply").onclick=...;
+//document.getElementById("#operation_divide").onclick=...;
+
+// let buttons = document.getElementsByClassName(".buttons");
+// for(let i = 0; i < buttons.length; i++){
+//     buttons[i].onclick = () => actualizeDisplay();
+// }
