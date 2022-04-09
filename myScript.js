f432c8b73225e8bd7dd75055aebe8dbe1bfd1584
SaToGo <stoledano97@gmail.com>
2022-04-09 14:55:20 +0200

    decimal point can be introduced now, savePoint function

--- myScript.js
+++ myScript.js
@@ -1,41 +1,50 @@
 
-// we covert to Number, then we do operation save in variable c
-// and we convert to string again on the return.
-//a, b -> string with a number "3"
+// we covert to Number, then we do the operation, save in variable c
+// and convert to string again on the return.
+//a, b -> string with a number ex. "3"
+//return a string with a number 
 function add(a, b) {
     a = Number(a);
     b = Number(b);
     let c = a + b;
     return '' + c;
+    // return c.toFixed(5);
 }
 
-//a, b -> string with a number "3"
+//a, b -> string with a number ex. "3"
+//return a string with a number.
 function subtract(a, b){
     a = Number(a);
     b = Number(b);
     let c = a - b;
     return '' + c;
+    // return c.toFixed(5);
 }
 
-//a, b -> string with a number "3"
+//a, b -> string with a number ex. "3"
+//return a string with a number.
 function multiply(a, b){
     a = Number(a);
     b = Number(b);
     let c = a*b;
     return '' + c;
+    // return c.toFixed(5);
 }
 
-//a, b -> string with a number "3"
+//a, b -> string with a number ex. "3"
+//return a string with a number.
 function divide(a, b){
     a = Number(a);
     b = Number(b);
     if(b == 0) return 'ERROR';
     let c = a/b;
     return '' + c;
+    // return c.toFixed(5);
 }
 
-//operation -> string with operation "add", ...
-//a, b -> string with a number "3"
+//operation -> string with operation ex. "add", ...
+//a, b -> string with a number ex. "3"
+//return a string with a number.
 function operate(operation, a, b){
     if(operation == "add") return add(a, b);
     else if(operation == "subtract") return subtract(a, b);
@@ -43,14 +52,14 @@ function operate(operation, a, b){
     else if(operation == "divide") return divide(a, b);
 }
 
-//string -> save the numbers that we will operate.
+//string -> save the numbers that we show in display and that we will operate.
 let displayValue2 = "0";
 let displayValue = "0";
 
-//string -> save operation "add", ...
+//string -> save the operations ex. "add", ...
 let lastOperation = "";
 let operation = "";
-//string -> save operation "+", ... to show in the display.
+//string -> save operation to show in the display. ex. "+", ...
 let displayOperation = "";
 
 //HTML element of the display, actual number.
@@ -72,10 +81,26 @@ function actualizeDisplay(result){
     }
 }
 
+//add number to our actual number in display.
 //value --> string that contains a number.
 function saveNumbers(value){
     displayValue = displayValue + value;
     if(displayValue === "0" + value) displayValue = value; //doesn't save the default 0.
+    actualizeDisplay(displayValue);
+}
+
+//add point to our actual number in display.
+//value --> string that should always be "."
+function savePoint(value) {
+    //summation of all the points in our string.
+    let total = 0;
+    let numbers = displayValue.split("");
+    numbers.forEach( char1 => { if(char1 == ".") total++; } );
+
+    //We only want 1 decimal point in our number.
+    if(total == 0 ){
+        displayValue = displayValue + value;
+    }
     actualizeDisplay(displayValue);
 }
 
@@ -120,7 +145,7 @@ function clear(){
     actualizeDisplay(displayValue);
 }
 
-function equal(){
+function equalOperation(){
     if(operation == ""){
         return 0;
     }
@@ -133,6 +158,7 @@ function equal(){
     }
 }
 
+// HTML BUTTONS 
 document.getElementById("number_7").onclick= () => saveNumbers('7');
 document.getElementById("number_8").onclick= () => saveNumbers('8');
 document.getElementById("number_9").onclick= () => saveNumbers('9');
@@ -147,8 +173,8 @@ document.getElementById("number_3").onclick= () => saveNumbers('3');
 
 document.getElementById("number_0").onclick= () => saveNumbers('0');
 
-//document.getElementById("number_point").onclick=...;
-document.getElementById("number_equal").onclick= () => equal();
+document.getElementById("number_point").onclick= () => savePoint('.');
+document.getElementById("number_equal").onclick= () => equalOperation();
 
 document.getElementById("operation_clear").onclick= () => clear();
 
@@ -156,8 +182,3 @@ document.getElementById("operation_plus").onclick= () => saveOperation("add");
 document.getElementById("operation_minus").onclick= () => saveOperation("subtract");
 document.getElementById("operation_multiply").onclick= () => saveOperation("multiply");
 document.getElementById("operation_divide").onclick= () => saveOperation("divide");
-
-// let buttons = document.getElementsByClassName("buttons");
-// for(let i = 0; i < buttons.length; i++){
-//     buttons[i].onclick = () => actualizeDisplay();
-// }
